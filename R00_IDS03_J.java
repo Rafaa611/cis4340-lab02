bf035104344b7295a51c5496444f871efb411577
Rafael Matos <69061465+Rafaa611@users.noreply.github.com>
2026-03-08 18:03:26 -0400

    Fix IDS03-J vulnerability by sanitizing user input

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,22 +1,33 @@
 /*
 Rule: IDS03-J
-Noncompliant version
-Logging unsanitized user input
+Compliant version
 */
 
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 public class R00_IDS03_J {
 
     private static final Logger logger =
             Logger.getLogger(R00_IDS03_J.class.getName());
 
+    public static String sanitizeUser(String username) {
+
+        if (Pattern.matches("[A-Za-z0-9]+", username)) {
+            return username;
+        }
+
+        return "unauthorized user";
+    }
+
     public static void logUser(boolean loginSuccessful, String username) {
 
+        String sanitized = sanitizeUser(username);
+
         if (loginSuccessful) {
-            logger.severe("User login succeeded for: " + username);
+            logger.severe("User login succeeded for: " + sanitized);
         } else {
-            logger.severe("User login failed for: " + username);
+            logger.severe("User login failed for: " + sanitized);
         }
 
     }
